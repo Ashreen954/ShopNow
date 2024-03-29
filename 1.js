9d8e55eb066d84894be67bb8001ca319fccd95d3
Ashreen954 <108571624+Ashreen954@users.noreply.github.com>
2024-03-30 00:07:11 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slide = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slide.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slide[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
